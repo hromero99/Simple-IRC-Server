2ab3eb6c9715cb0a7a8a98710abc559ff7e7e951
hromero99 <hromerolopez@protonmail.com>
2020-10-14 17:24:44 +0200

    Added Channel string info to Client software

--- include/client.hpp
+++ include/client.hpp
@@ -8,6 +8,7 @@
 #include <signal.h>
 #include <unistd.h>
 #include <time.h>
+#include <string.h>
 #include <arpa/inet.h>
 #include <string>
 #include <vector>
@@ -19,6 +20,7 @@ private:
     socklen_t _serverAddressLength;
     fd_set _readfds;
     fd_set _auxfds;
+    std::string _channel;
 public:
     Client(std::string serverAddress, int serverPort);
     void connectIRCServer();
@@ -26,6 +28,8 @@ public:
     bool validateMessage(std::string message);
     void sendMessage(const std::string &message);
     int lookForConnections();
+    inline void setChannel(std::string channel){_channel = channel;};
+    inline std::string getChannel()const{return _channel;};
     std::string checkIfHasMessages();
 };
 
